72228b09b78e07111ca8abe7a5fad3247a1f29a7
Sadra <84704927+sadra-hub@users.noreply.github.com>
2024-11-29 07:35:59 +0100

    ✨ feat(drawHeatmap): add function to extract shape coordinates
    - create get_shapes function to retrieve x, y coordinates of rectangle shapes from XML file
    - include logic to identify polygons connecting rectangles and extract their coordinates

---
+++ drawHeatmap.py
@@ -0,0 +1,12 @@
+def get_shapes(rect_refid_list, diagram_xml_file):
+    # receive rect_refid_list which contains a list of refiD for shapes of rectangles
+    # First it identifies the x and y for the rectangle shapes in the XML file
+    # if there's more than two rect shapes it search to see if there's a poly that
+    # connect these two rect, if there is a poly, the x and y of that poly is added to returned list
+    # 
+    # 
+    # return a list of x,y cooordinates for rect and poly connecting rect to each other
+    # for each item in the returned list 4 (x,y) cooridicates.
+    refId_coordinates = []
+    
+    return refId_coordinates
